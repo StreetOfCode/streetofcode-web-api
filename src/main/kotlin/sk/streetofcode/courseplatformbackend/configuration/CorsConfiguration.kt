b817a553b1932a8eba7ac707aaffa1814f488f82
gabrielkerekes <gabriel.kerekes@vacuumlabs.com>
2020-10-10 16:59:12 +0200

    Add CorsConfiguraiton

---
+++ src/main/kotlin/sk/streetofcode/courseplatformbackend/configuration/CorsConfiguration.kt
@@ -0,0 +1,12 @@
+package sk.streetofcode.courseplatformbackend.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfiguration : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**").allowedOrigins("*")
+    }
+}
