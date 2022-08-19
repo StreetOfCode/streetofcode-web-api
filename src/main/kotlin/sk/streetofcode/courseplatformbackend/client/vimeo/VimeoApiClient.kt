7ea369e2067d1ad6d297b2de30f11cfac14ddebc
xjahic <jahic.jakub@gmail.com>
2022-08-19 22:28:38 +0200

    Fix bug in VimeoApiClient

--- src/main/kotlin/sk/streetofcode/courseplatformbackend/client/vimeo/VimeoApiClient.kt
+++ src/main/kotlin/sk/streetofcode/courseplatformbackend/client/vimeo/VimeoApiClient.kt
@@ -1,25 +1,18 @@
 package sk.streetofcode.courseplatformbackend.client.vimeo
 
 import org.json.JSONObject
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForEntity
-import sk.streetofcode.courseplatformbackend.client.vimeo.configuration.VimeoProperties
 
 @Service
 class VimeoApiClient(
     @Qualifier("vimeoRestTemplate") private val restTemplate: RestTemplate,
-    private val properties: VimeoProperties
 ) {
 
-    companion object {
-        private val log = LoggerFactory.getLogger(VimeoApiClient::class.java)
-    }
-
     fun getVideoDurationInSeconds(videoId: String? = null): Int {
-        if (videoId == null) {
+        if (videoId.isNullOrEmpty()) {
             return 0
         }
 
