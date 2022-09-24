8793d3326d57eb58096fba6b0c15e3fc25fc1172
xjahic <jahic.jakub@gmail.com>
2022-09-24 18:49:05 +0200

    Fix integration tests

--- src/main/resources/db/specific/local/V1.2__difficulties.sql
+++ src/main/resources/db/specific/local/V1.2__difficulties.sql
@@ -1,3 +1,3 @@
-INSERT INTO public.difficulty (id, name, skill_level) VALUES (1, 'Začiatočník', 1);
-INSERT INTO public.difficulty (id, name, skill_level) VALUES (2, 'Mierne pokročilý', 2);
-INSERT INTO public.difficulty (id, name, skill_level) VALUES (3, 'Pokročilý', 3);
+INSERT INTO public.difficulty (id, name, skill_level) VALUES (difficulty_id_seq.nextval, 'Začiatočník', 1);
+INSERT INTO public.difficulty (id, name, skill_level) VALUES (difficulty_id_seq.nextval, 'Mierne pokročilý', 2);
+INSERT INTO public.difficulty (id, name, skill_level) VALUES (difficulty_id_seq.nextval, 'Pokročilý', 3);
