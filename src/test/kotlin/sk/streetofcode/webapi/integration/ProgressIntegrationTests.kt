c11ef8fcae16e1457396e575683832c85ef75625
xjahic <jahic.jakub@gmail.com>
2022-09-25 17:01:01 +0200

    Fix bug in progress service and adapt integration tests

--- src/test/kotlin/sk/streetofcode/webapi/integration/ProgressIntegrationTests.kt
+++ src/test/kotlin/sk/streetofcode/webapi/integration/ProgressIntegrationTests.kt
@@ -5,12 +5,19 @@ import io.kotest.matchers.shouldNotBe
 import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.boot.test.web.client.postForEntity
 import org.springframework.http.HttpStatus
+import sk.streetofcode.webapi.api.dto.ChapterDto
+import sk.streetofcode.webapi.api.dto.CourseDto
+import sk.streetofcode.webapi.api.dto.LectureDto
 import sk.streetofcode.webapi.api.dto.progress.CourseProgressOverviewDto
 import sk.streetofcode.webapi.api.dto.progress.UserProgressMetadataDto
 import sk.streetofcode.webapi.api.exception.BadRequestException
 import sk.streetofcode.webapi.api.exception.ResourceNotFoundException
+import sk.streetofcode.webapi.api.request.ChapterAddRequest
+import sk.streetofcode.webapi.api.request.CourseAddRequest
+import sk.streetofcode.webapi.api.request.LectureAddRequest
 import sk.streetofcode.webapi.api.request.ResetProgressDto
 import sk.streetofcode.webapi.configuration.SpringBootTestAnnotation
+import sk.streetofcode.webapi.model.CourseStatus
 import sk.streetofcode.webapi.model.progress.ProgressStatus
 
 @SpringBootTestAnnotation
@@ -18,21 +25,21 @@ class ProgressIntegrationTests : IntegrationTests() {
 
     init {
         "complex progress tracking test" {
-            val courseId = 1L
+            val courseId = createCourseForComplexProgressTest()
 
             // progress metadata is not found yet
             getProgressMetadataNotFound(courseId)
 
             // check progressOverview - everything is not seen yet
-            val progressOverview = getProgressOverview(courseId)
-            progressOverview.lecturesViewed shouldBe 0
-            progressOverview.chapters.forEach { it ->
+            val initialProgressOverview = getProgressOverview(courseId)
+            initialProgressOverview.lecturesViewed shouldBe 0
+            initialProgressOverview.chapters.forEach { it ->
                 it.viewed shouldBe false
                 it.lectures.forEach { it.viewed shouldBe false }
             }
 
-            // update progress, seen lectureId 1
-            updateProgress(1)
+            // update progress, seen first lecture
+            updateProgress(initialProgressOverview.chapters[0].lectures[0].id)
 
             // verify seen lecture
             val progressOverviewLectureSeen = getProgressOverview(courseId)
@@ -46,12 +53,12 @@ class ProgressIntegrationTests : IntegrationTests() {
             progressMetadataLectureSeen.lecturesViewed shouldBe 1
             progressMetadataLectureSeen.status shouldBe ProgressStatus.IN_PROGRESS
             progressMetadataLectureSeen.finishedAt shouldBe null
-            progressMetadataLectureSeen.nextChapterId shouldBe 1
-            progressMetadataLectureSeen.nextLectureId shouldBe 2
+            progressMetadataLectureSeen.nextChapterId shouldBe initialProgressOverview.chapters[0].id
+            progressMetadataLectureSeen.nextLectureId shouldBe initialProgressOverview.chapters[0].lectures[1].id
 
-            // update progress, seen lectureId 2, 3
-            updateProgress(2)
-            updateProgress(3)
+            // update progress, seen rest of the lectures in first chapter
+            updateProgress(initialProgressOverview.chapters[0].lectures[1].id)
+            updateProgress(initialProgressOverview.chapters[0].lectures[2].id)
 
             // verify seen chapter
             val progressOverviewChapterSeen = getProgressOverview(courseId)
@@ -67,13 +74,13 @@ class ProgressIntegrationTests : IntegrationTests() {
             progressMetadataChapterSeen.lecturesViewed shouldBe 3
             progressMetadataChapterSeen.status shouldBe ProgressStatus.IN_PROGRESS
             progressMetadataChapterSeen.finishedAt shouldBe null
-            progressMetadataChapterSeen.nextChapterId shouldBe 2
-            progressMetadataChapterSeen.nextLectureId shouldBe 4
+            progressMetadataChapterSeen.nextChapterId shouldBe initialProgressOverview.chapters[1].id
+            progressMetadataChapterSeen.nextLectureId shouldBe initialProgressOverview.chapters[1].lectures[0].id
 
             // reset progress lecture
-            resetProgress(ResetProgressDto(lectureId = 3))
+            resetProgress(ResetProgressDto(lectureId = initialProgressOverview.chapters[0].lectures[2].id))
 
-            // verify changed progress overview - chapter is not seen, lecture 1,2 are seen
+            // verify changed progress overview - chapter is not seen, first and second lectures are seen
             val progressOverviewLectureReset = getProgressOverview(courseId)
             progressOverviewLectureReset.lecturesViewed shouldBe 2
             progressOverviewLectureReset.chapters[0].viewed shouldBe false
@@ -81,8 +88,8 @@ class ProgressIntegrationTests : IntegrationTests() {
             progressOverviewLectureReset.chapters[0].lectures[1].viewed shouldBe true
             progressOverviewLectureReset.chapters[0].lectures[2].viewed shouldBe false
 
-            // update progress - lecture 3
-            updateProgress(3)
+            // update progress - see last lecture in first chapter
+            updateProgress(initialProgressOverview.chapters[0].lectures[2].id)
 
             // verify changed progress overview - chapter is seen again
             val progressOverviewChapterSeenAgain = getProgressOverview(courseId)
@@ -93,7 +100,7 @@ class ProgressIntegrationTests : IntegrationTests() {
             progressOverviewChapterSeenAgain.chapters[0].lectures[2].viewed shouldBe true
 
             // reset progress chapter
-            resetProgress(ResetProgressDto(chapterId = 1))
+            resetProgress(ResetProgressDto(chapterId = initialProgressOverview.chapters[0].id))
 
             // verify that everything is unseen again
             val progressOverviewChapterReset = getProgressOverview(courseId)
@@ -108,12 +115,12 @@ class ProgressIntegrationTests : IntegrationTests() {
             progressMetadataReset.lecturesViewed shouldBe 0
             progressMetadataReset.status shouldBe ProgressStatus.IN_PROGRESS
             progressMetadataReset.finishedAt shouldBe null
-            progressMetadataReset.nextChapterId shouldBe 1
-            progressMetadataReset.nextLectureId shouldBe 1
+            progressMetadataReset.nextChapterId shouldBe initialProgressOverview.chapters[0].id
+            progressMetadataReset.nextLectureId shouldBe initialProgressOverview.chapters[0].lectures[0].id
 
             // update progress - see whole course
-            for (i in 1..5) {
-                updateProgress(i.toLong())
+            for (i in initialProgressOverview.chapters[0].lectures[0].id..initialProgressOverview.chapters[1].lectures[1].id) {
+                updateProgress(i)
             }
 
             // verify changed progress overview - whole course seen
@@ -131,7 +138,7 @@ class ProgressIntegrationTests : IntegrationTests() {
             progressMetadataCourseSeen.finishedAt shouldNotBe null
 
             // reset progress course
-            resetProgress(ResetProgressDto(courseId = 1))
+            resetProgress(ResetProgressDto(courseId))
 
             // verify progress overview - reset course
             val progressOverviewCourseReset = getProgressOverview(courseId)
@@ -146,13 +153,49 @@ class ProgressIntegrationTests : IntegrationTests() {
             progressMetadataResetAgain.lecturesViewed shouldBe 0
             progressMetadataResetAgain.status shouldBe ProgressStatus.IN_PROGRESS
             progressMetadataResetAgain.finishedAt shouldBe null
-            progressMetadataResetAgain.nextChapterId shouldBe 1
-            progressMetadataResetAgain.nextLectureId shouldBe 1
+            progressMetadataResetAgain.nextChapterId shouldBe initialProgressOverview.chapters[0].id
+            progressMetadataResetAgain.nextLectureId shouldBe initialProgressOverview.chapters[0].lectures[0].id
         }
 
         "fail reset progress, bad request" {
             resetProgressBadRequest(ResetProgressDto()) // everything is null in request
         }
+    }
+
+    // Creates course with 2 chapters and 5 lectures (3 lectures in chapter 1 and 2 lectures in chapter 2)
+    // Returns course id
+    private fun createCourseForComplexProgressTest(): Long {
+        val courseAddRequest = CourseAddRequest(1, 1, "test", "test", "test", "short", null, "url", null, "url", CourseStatus.PUBLIC)
+        val courseId = restWithAdminRole().postForEntity<CourseDto>("/course", courseAddRequest).let {
+            it.statusCode shouldBe HttpStatus.CREATED
+            it.body!!.id
+        }
+
+        val chapterAddRequest = ChapterAddRequest(courseId, "test", 1)
+        val chapterIdFirst = restWithAdminRole().postForEntity<ChapterDto>("/chapter", chapterAddRequest).let {
+            it.statusCode shouldBe HttpStatus.CREATED
+            it.body!!.id
+        }
+
+        val chapterAddRequest2 = ChapterAddRequest(courseId, "test", 2)
+        val chapterIdSecond = restWithAdminRole().postForEntity<ChapterDto>("/chapter", chapterAddRequest2).let {
+            it.statusCode shouldBe HttpStatus.CREATED
+            it.body!!.id
+        }
+
+        val lecture1ForChapter1 = LectureAddRequest(chapterIdFirst, "test", 1, "content")
+        val lecture2ForChapter1 = LectureAddRequest(chapterIdFirst, "test", 2, "content")
+        val lecture3ForChapter1 = LectureAddRequest(chapterIdFirst, "test", 3, "content")
+        restWithAdminRole().postForEntity<LectureDto>("/lecture", lecture1ForChapter1)
+        restWithAdminRole().postForEntity<LectureDto>("/lecture", lecture2ForChapter1)
+        restWithAdminRole().postForEntity<LectureDto>("/lecture", lecture3ForChapter1)
+
+        val lecture1ForChapter2 = LectureAddRequest(chapterIdSecond, "test", 1, "content")
+        val lecture2ForChapter2 = LectureAddRequest(chapterIdSecond, "test", 2, "content")
+        restWithAdminRole().postForEntity<LectureDto>("/lecture", lecture1ForChapter2)
+        restWithAdminRole().postForEntity<LectureDto>("/lecture", lecture2ForChapter2)
+
+        return courseId
     }
 
     private fun getProgressOverview(courseId: Long): CourseProgressOverviewDto {
@@ -182,6 +225,9 @@ class ProgressIntegrationTests : IntegrationTests() {
     }
 
     private fun resetProgressBadRequest(body: ResetProgressDto) {
-        return restWithAdminRole().postForEntity<BadRequestException>("/progress/reset", body).statusCode shouldBe HttpStatus.BAD_REQUEST
+        return restWithAdminRole().postForEntity<BadRequestException>(
+            "/progress/reset",
+            body
+        ).statusCode shouldBe HttpStatus.BAD_REQUEST
     }
 }
