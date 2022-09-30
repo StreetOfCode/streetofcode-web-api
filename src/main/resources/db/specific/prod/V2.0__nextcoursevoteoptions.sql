e122a28a585ffddc86b442a485e142f977228818
xjahic <jahic.jakub@gmail.com>
2022-09-30 16:45:01 +0200

    New migration - add next course vote option data

---
+++ src/main/resources/db/specific/prod/V2.0__nextcoursevoteoptions.sql
@@ -0,0 +1,11 @@
+INSERT INTO next_course_vote_option(id, name) VALUES
+(nextval('next_course_vote_option_id_seq'), 'Základy Javy'),
+(nextval('next_course_vote_option_id_seq'), 'Základy C#'),
+(nextval('next_course_vote_option_id_seq'), 'Projekty v Javascripte'),
+(nextval('next_course_vote_option_id_seq'), 'Projekty v Pythone'),
+(nextval('next_course_vote_option_id_seq'), 'Projekty v Kotline'),
+(nextval('next_course_vote_option_id_seq'), 'Projekty v C#'),
+(nextval('next_course_vote_option_id_seq'), 'Základy Git-u'),
+(nextval('next_course_vote_option_id_seq'), 'SQL/Databázy'),
+(nextval('next_course_vote_option_id_seq'), 'Základy kryptografie'),
+(nextval('next_course_vote_option_id_seq'), 'Algoritmy a dátové štruktúry');
