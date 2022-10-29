32100dab7e3b753b5772d842a83ec5fa6e612770
xjahic <jahic.jakub@gmail.com>
2022-10-29 11:19:51 +0200

    Fix failing test

--- src/main/kotlin/sk/streetofcode/webapi/configuration/SentryBeforeCallback.kt
+++ src/main/kotlin/sk/streetofcode/webapi/configuration/SentryBeforeCallback.kt
@@ -10,7 +10,7 @@ class SentryBeforeCallback : SentryOptions.BeforeSendCallback {
     override fun execute(event: SentryEvent, hint: Hint): SentryEvent? {
         if (event.throwable is org.springframework.security.access.AccessDeniedException) {
             // Don't send errors which are type of AccessDeniedException.
-            // These errors are created when an unauthorized user tries to call an API which has IsAdmin annotation.
+            // These errors are created when an unauthorized user tries to call an API which has IsAuthenticated annotation.
             return null
         }
 
