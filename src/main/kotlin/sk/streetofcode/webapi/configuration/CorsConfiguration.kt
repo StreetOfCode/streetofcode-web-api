67452c6575eca1217f169d7313096c5ae117d63a
xjahic <jahic.jakub@gmail.com>
2022-11-22 17:55:00 +0100

    Update CORS configuration

--- src/main/kotlin/sk/streetofcode/webapi/configuration/CorsConfiguration.kt
+++ src/main/kotlin/sk/streetofcode/webapi/configuration/CorsConfiguration.kt
@@ -1,13 +1,17 @@
 package sk.streetofcode.webapi.configuration
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class CorsConfiguration : WebMvcConfigurer {
+
+    @Value("\${cors.allowed-origin-patterns}")
+    private lateinit var allowedOriginPatterns: List<String>
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**").allowedOrigins("*")
+        registry.addMapping("/**").allowedOriginPatterns(*allowedOriginPatterns.toTypedArray())
             .exposedHeaders("Content-Range")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "PATCH", "OPTIONS")
     }
