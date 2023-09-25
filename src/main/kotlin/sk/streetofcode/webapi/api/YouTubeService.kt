93dc63e623d5fbad7d1351f6ae712113e04e6556
kubo <jsodpad@gmail.com>
2023-09-25 22:52:41 +0200

    Add support for fetching our YouTube videos

---
+++ src/main/kotlin/sk/streetofcode/webapi/api/YouTubeService.kt
@@ -0,0 +1,8 @@
+package sk.streetofcode.webapi.api
+
+import com.google.api.services.youtube.model.Video
+
+interface YouTubeService {
+    fun getVideos(): List<Video>
+    fun getVideo(id: String): Video
+}
